29ba5d312ea346811985b247bcd0011fb694705c
HiteshSukhpal03 <hiteshsukhpal03@gmail.com>
2024-10-17 00:57:07 +0530

    Fixed scroll issue

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -49,7 +49,7 @@ const DashboardPage = () => {
       </div>
       <div className="w-full min-h-screen" onClick={handleClick} onTouchMove={(e)=>{slideHandler(e)}}>
         <Topbar toggleExpansionSwitch={toggleExpansionSwitch} toggle={toggle}/>
-         <div className="bg-BgPrimaryColor px-5 py-2 overflow-y-scroll h-[80svh]">
+         <div className="bg-BgPrimaryColor px-5 py-2 overflow-y-scroll h-[90svh] content-scroll-bar">
           <Suspense fallback={<Loading loadingText={t("loading")} />}>
             <Routes>
               <Route path="/" element={<CompanyList />} />
