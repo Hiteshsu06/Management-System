b5719b8b9e464d3cccb678def30a9f2be40c8c9e
HiteshSukhpal03 <hiteshsukhpal03@gmail.com>
2024-10-16 23:36:41 +0530

    fixed loading height

--- src/components/common/Loading.jsx
+++ src/components/common/Loading.jsx
@@ -2,7 +2,7 @@ import { ProgressSpinner } from 'primereact/progressspinner';
 
 const Loading = () => {
   return (
-    <div className="flex min-h-screen items-center justify-content-center absolute w-full z-10 bg-[rgb(236 236 241 / 50%)]">
+    <div className="flex items-center justify-content-center absolute w-full z-10 bg-[rgb(236 236 241 / 50%)]" style={{ height: '100svh' }}>
         <ProgressSpinner style={{width: '50px', height: '50px'}} className='text-BgTertiaryColor'/>
     </div>
   )
